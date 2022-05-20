fd545a38ad17ea2b7262fe6c11caa64a4ed67071
JavanShen <1985486089@qq.com>
2022-05-20 22:05:26 +0800

    新增背景组件

---
+++ src/components/Background.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg" :style="{backgroundImage:'url('+src+')'}" ></div>
+</template>
+
+<script setup>
+defineProps({
+    src: {
+        default: ''
+    }
+})
+</script>
+
+<style scoped>
+.bg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    filter: blur(10px);
+    transform: scale(1.1);
+    z-index: -1;
+}
+</style>
