3f539c6e44b2fda6890ef32190eb1f7c25a1b29a
JavanShen <1985486089@qq.com>
2022-05-20 22:03:26 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from './ConfigProvide.vue';
 import router from './router';
-import store from './store';
+import '@/styles'
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(router).mount('#app');
