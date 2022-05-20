81de64d43c9ee0edb118c61e611044346025d052
JavanShen <1985486089@qq.com>
2022-05-20 22:05:04 +0800

    中文化

---
+++ src/ConfigProvide.vue
@@ -0,0 +1,13 @@
+<!-- naive全局配置 -->
+<template>
+    <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+        <n-message-provider>
+            <App />
+        </n-message-provider>
+    </n-config-provider>
+</template>
+
+<script setup>
+import { zhCN, dateZhCN } from 'naive-ui'
+import App from './App.vue'
+</script>
