234df0ac08c05d7d7a2cfd21f548fed450366b16
JavanShen <1985486089@qq.com>
2022-05-23 16:56:22 +0800

    路由拦截判断状态

---
+++ src/permission.js
@@ -0,0 +1,43 @@
+import router from '@/router';
+import {usePermissionStore,useUserStore} from '@/store'
+
+import { getToken } from '@/utils/auth';
+
+router.beforeEach(async (to, from, next) => {
+    if (to.path === '/login') {
+        next();
+    } else {
+        const token = getToken();
+        if (token === undefined) {
+            next({ name: 'Login' });
+        } else {
+            const permission = usePermissionStore()
+            const user = useUserStore()
+
+            if (user.roles?.length > 0) {
+                next();
+            } else {
+                try {
+                    const {roles} = await user.getUserInfo(token)
+                    const routes = await permission.generateRoutes(roles)
+
+                    routes.forEach(route => router.addRoute(route))
+                    router.addRoute(
+                        {
+                            path: '/.*',
+                            name: 'NotFound',
+                            component: () => import('@/views/notFound.vue'),
+                        }
+                    )
+
+                    next({ ...to, replace: true });
+                } catch (e) {
+                    console.log(e)
+                    next({ name: 'Login' });
+                }
+            }
+        }
+    }
+});
+
+export default router;
