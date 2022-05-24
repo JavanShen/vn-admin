6a81eacdb32384be72a1b0b6e2fd27b58b869cf6
JavanShen <1985486089@qq.com>
2022-05-24 09:11:30 +0800

    处理svg文件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const {NaiveUiResolver} = require('unplugin-vue-components/resolvers');
 
+const path=require('path');
+const resolve=(dir) => {
+    return path.join(__dirname, dir);
+}
+
 module.exports= {
     lintOnSave: false,
     configureWebpack: {
@@ -8,5 +13,23 @@ module.exports= {
                 resolvers: [NaiveUiResolver()]
             }),
         ]
+    },
+    chainWebpack: config=>{
+        config.module
+        .rule('svg')
+        .exclude.add(resolve('src/icon'))
+        .end()
+
+        config.module
+        .rule('icons')
+        .test(/\.svg$/)
+        .include.add(resolve('src/icon'))
+        .end()
+        .use('svg-sprite-loader')
+        .loader('svg-sprite-loader')
+        .options({
+            symbolId: 'icon-[name]'
+        })
+        .end()
     }
 }
