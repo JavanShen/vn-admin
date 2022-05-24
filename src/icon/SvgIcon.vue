6c53f636ef1d92de9bd5fafac8c7e4029918f18d
JavanShen <1985486089@qq.com>
2022-05-24 09:10:53 +0800

    svg sprite

---
+++ src/icon/SvgIcon.vue
@@ -0,0 +1,28 @@
+<template>
+    <svg class="icon" aria-hidden="true">
+        <use :xlink:href="icon"></use>
+    </svg>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    iconClass: {
+        type: String,
+        required: true
+    }
+})
+
+const icon = computed(() => `#icon-${props.iconClass}`)
+</script>
+
+<style scoped>
+.icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
+</style>
