96196dfe8cc00bac7295792c23b0a653587164cb
JavanShen <1985486089@qq.com>
2022-05-25 20:22:29 +0800

    :lipstick:  添加图标和回到首页按钮

--- src/views/notFound.vue
+++ src/views/notFound.vue
@@ -1,7 +1,41 @@
 <template>
-    404
+    <n-empty class="empty" size="huge" description="啊哦, 这个页面找不到了">
+        <template #icon>
+            <div class="icon">
+                <svg-icon icon-class="404"></svg-icon>
+            </div>
+        </template>
+        <template #extra>
+            <n-button size="large" @click="backHome">
+                回到首页
+            </n-button>
+        </template>
+    </n-empty>
 </template>
 
 <script setup>
+import router from "@/router";
 
+const backHome = () => {
+    router.push({ path: '/', replace: true });
+}
 </script>
+
+<style scoped>
+.icon {
+    height: 250px;
+    width: 600px;
+}
+
+.empty {
+    position: fixed;
+    top: 42%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.empty :deep(.n-empty__icon) {
+    height: auto;
+    width: auto;
+}
+</style>
