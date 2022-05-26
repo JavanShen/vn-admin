ce4f8e1889a1f630bb3f7ac879f6177dab8e0849
JavanShen <1985486089@qq.com>
2022-05-26 20:37:24 +0800

    :tada: 用户列表页

---
+++ src/views/user/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <Table :columns="columns" :table-data="tableData" :loading="loading" :isEdit="true" />
+</template>
+
+<script setup>
+import Table from "./components/Table";
+import columns from "./utils/columns";
+import useTableData from '@/composables/user/useTableData';
+
+const {tableData, getData, loading} = useTableData();
+</script>
+
+<style scoped>
+.pagination {
+    margin-top: 20px;
+}
+</style>
