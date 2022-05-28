a85603ef81fa8faed3158c803d7f448d6a779eca
JavanShen <1985486089@qq.com>
2022-05-28 22:47:32 +0800

    :hammer: 添加新的检测方法

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,3 +1,11 @@
-export const isExternal=(path) => {
+export const isExternal = (path) => {
     return /^(https?:|mailto:|tel:)/.test(path);
+}
+
+export const isEmail = (email) => {
+    return /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/.test(email);
+}
+
+export const isPhone = (phone) => {
+    return /^1[3456789]\d{9}$/.test(phone);
 }
