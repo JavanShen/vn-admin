349dd2e163acd77afec96981360981b3a917a879
JavanShen <1985486089@qq.com>
2022-05-31 13:37:13 +0800

    :hammer: 添加判断相同对象方法

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -9,3 +9,28 @@ export const isEmail = (email) => {
 export const isPhone = (phone) => {
     return /^1[3456789]\d{9}$/.test(phone);
 }
+
+export function isSameObject(obj1,obj2,isStrict=false){
+    if(isStrict){
+        return JSON.stringify(obj1)===JSON.stringify(obj2);
+    }else{
+        const isSame=(obj)=>{
+            for(let key in obj){
+                if(typeOf(obj[key])==='object'){
+                    if(!isSame(obj[key])){
+                        return false;
+                    }
+                }else if(obj[key]!==obj2[key]){
+                    return false;
+                }
+            }
+            return true
+        }
+
+        return isSame(obj1);
+    }
+}
+
+export function typeOf(obj){
+    return Object.prototype.toString.call(obj).slice(8,-1).toLowerCase();
+}
