081b1cb1b90f5c77d3731b126c7344188d8618df
JavanShen <1985486089@qq.com>
2022-06-01 16:28:36 +0800

    :sparkles: 按钮级权限控制

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import router from './router';
 import { createPinia } from 'pinia';
 
 import './permission'
+import permissionDirective from '@/directive/permission';
 
 import 'animate.css'
 import '@/styles'
 import SvgIcon from '@/icon'
 import '../mock/api';
 
-export const app = createApp(App).use(createPinia()).use(router).use(SvgIcon).mount('#app');
+export const app = createApp(App)
+.use(createPinia())
+.use(router)
+.use(SvgIcon)
+.use(permissionDirective)
+.mount('#app');
