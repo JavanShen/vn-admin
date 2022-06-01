560ba4ab95b2baeb8b0861ec3d5422a58f3eb8e2
JavanShen <1985486089@qq.com>
2022-06-01 16:30:01 +0800

    :hammer: 添加数组相交判断

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -10,17 +10,17 @@ export const isPhone = (phone) => {
     return /^1[3456789]\d{9}$/.test(phone);
 }
 
-export function isSameObject(obj1,obj2,isStrict=false){
-    if(isStrict){
-        return JSON.stringify(obj1)===JSON.stringify(obj2);
-    }else{
-        const isSame=(obj)=>{
-            for(let key in obj){
-                if(typeOf(obj[key])==='object'){
-                    if(!isSame(obj[key])){
+export function isSameObject(obj1, obj2, isStrict = false) {
+    if (isStrict) {
+        return JSON.stringify(obj1) === JSON.stringify(obj2);
+    } else {
+        const isSame = (obj) => {
+            for (let key in obj) {
+                if (typeOf(obj[key]) === 'object') {
+                    if (!isSame(obj[key])) {
                         return false;
                     }
-                }else if(obj[key]!==obj2[key]){
+                } else if (obj[key] !== obj2[key]) {
                     return false;
                 }
             }
@@ -31,6 +31,20 @@ export function isSameObject(obj1,obj2,isStrict=false){
     }
 }
 
-export function typeOf(obj){
-    return Object.prototype.toString.call(obj).slice(8,-1).toLowerCase();
+export function typeOf(obj) {
+    return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+}
+
+export function isIntersect(arr1 = [], arr2 = []) {
+    let map = new Map()
+
+    for (let i = 0; i < arr1.length; i++) {
+        map.set(arr1[i], true)
+    }
+
+    for (let j = 0; j < arr2.length; j++) {
+        if (map.has(arr2[j])) {
+            return true;
+        }
+    }
 }
