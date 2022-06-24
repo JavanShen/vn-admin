ed7ed7e3d3a220f52cb84b7ae9e39b36cc9ff98f
JavanShen <1985486089@qq.com>
2022-06-24 16:28:29 +0800

    :wrench: 只在生成环境启动webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -2,38 +2,42 @@
 const { NaiveUiResolver } = require('unplugin-vue-components/resolvers');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-const path=require('path');
-const resolve=(dir) => {
+const path = require('path');
+const resolve = (dir) => {
     return path.join(__dirname, dir);
 }
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vn-admin/' : '/',
+    publicPath: isProd ? '/vn-admin/' : '/',
     lintOnSave: false,
     configureWebpack: {
         plugins: [
             require('unplugin-vue-components/webpack')({
                 resolvers: [NaiveUiResolver()]
             }),
-            new BundleAnalyzerPlugin(),
+            new BundleAnalyzerPlugin({
+                analyzerMode: isProd ? 'server' : 'disabled'
+            })
         ]
     },
-    chainWebpack: config=>{
+    chainWebpack: config => {
         config.module
-        .rule('svg')
-        .exclude.add(resolve('src/icon'))
-        .end()
+            .rule('svg')
+            .exclude.add(resolve('src/icon'))
+            .end()
 
         config.module
-        .rule('icons')
-        .test(/\.svg$/)
-        .include.add(resolve('src/icon'))
-        .end()
-        .use('svg-sprite-loader')
-        .loader('svg-sprite-loader')
-        .options({
-            symbolId: 'icon-[name]'
-        })
-        .end()
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(resolve('src/icon'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+            .end()
     }
 }
