d493ed237af2d7e8233fd05beb14ae606d986846
apine <1985486089@qq.com>
2022-09-19 16:20:28 +0800

    :wrench: 更新eslint配置以支持ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,45 @@
 /// 语法规范
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/vue3-essential", "@vue/airbnb", "plugin:prettier/recommended"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "import/no-extraneous-dependencies": [
-      "error",
-      {
-        devDependencies: true,
-      },
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/airbnb',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended'
     ],
-    "vue/multi-word-component-names": 0,
-    "no-restricted-syntax": [
-      "error",
-      {
-        selector: "ForInStatement",
-        message:
-          "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
-      },
-      {
-        selector: "LabeledStatement",
-        message:
-          "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
-      },
-      {
-        selector: "WithStatement",
-        message:
-          "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
-      },
-    ],
-  },
-};
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: true
+            }
+        ],
+        'vue/multi-word-component-names': 0,
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: 'ForInStatement',
+                message:
+                    'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.'
+            },
+            {
+                selector: 'LabeledStatement',
+                message:
+                    'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
+            },
+            {
+                selector: 'WithStatement',
+                message:
+                    '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
+            }
+        ]
+    }
+}
