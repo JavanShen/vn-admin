b32f6e8363c1077a7a676c939dba0c5a37356331
apine <1985486089@qq.com>
2022-09-20 16:12:10 +0800

    :wrench: 增加eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,15 @@ module.exports = {
                 devDependencies: true
             }
         ],
+        // 允许对指定入参进行修改
+        'no-param-reassign': [
+            'error',
+            {
+                props: true,
+                ignorePropertyModificationsFor: ['config']
+            }
+        ],
+        // 省略文件后缀
         'import/extensions': [
             'error',
             'ignorePackages',
@@ -33,6 +42,7 @@ module.exports = {
             }
         ],
         'vue/multi-word-component-names': 0,
+        // 使用for...of
         'no-restricted-syntax': [
             'error',
             {
@@ -50,8 +60,16 @@ module.exports = {
                 message:
                     '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
             }
+        ],
+        // 允许循环中的++
+        'no-plusplus': [
+            'error',
+            {
+                allowForLoopAfterthoughts: true
+            }
         ]
     },
+    // 解决报错unable resolve
     settings: {
         'import/resolver': {
             typescript: {}
