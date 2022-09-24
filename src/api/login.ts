850bd2f66d9ae98fd74579088b8f7c285f52e22c
apine <1985486089@qq.com>
2022-09-24 11:51:02 +0800

    :construction: 添加类型

---
+++ src/api/login.ts
@@ -0,0 +1,20 @@
+import service from '@/utils/request'
+import type { LoginInfo, UserInfo } from '@/interface/request/login'
+
+export function loginByUserName(info: LoginInfo) {
+    return service<{ token: string }>({
+        url: '/login',
+        method: 'post',
+        data: info
+    })
+}
+
+export function getUserInfo(token: string) {
+    return service<UserInfo>({
+        url: '/user/info',
+        method: 'post',
+        data: {
+            token
+        }
+    })
+}
